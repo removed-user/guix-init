ce173f4753e7f96419866f65dddaf9eb8b251431
casey <caseyriley94@gmail.com>
2026-06-28 01:14:04 -0400

    add parens

--- Channels/PerChannel/scheme/channels.scm
+++ Channels/PerChannel/scheme/channels.scm
@@ -1,6 +1,6 @@
 (list 
-load "/etc/guix/channels.d/guix.scm"
-load "/etc/guix/channels.d/nonguix.scm"
+(load "/etc/guix/channels.d/guix.scm")
+(load "/etc/guix/channels.d/nonguix.scm")
 ; load "/etc/guix/channels.d/extra/guix-science.scm"
 ; load "/etc/guix/channels.d/extra/guix-past.scm"
  )
