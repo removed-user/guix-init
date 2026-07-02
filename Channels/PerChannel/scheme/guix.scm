b0b50fc6f495c06bd0e48821c289329d2113a33c
casey <caseyriley94@gmail.com>
2026-07-02 17:45:17 -0400

    syntax ' to "

--- Channels/PerChannel/scheme/guix.scm
+++ Channels/PerChannel/scheme/guix.scm
@@ -7,4 +7,4 @@
 	; ## Commit by Vinicius Monego
       "27d13fbe68807eb7306770ac26ac8b10655cb9fd"
       (openpgp-fingerprint
-        'B845 5B7F FAD5 E8E9 5DEF  4296 637B 0B13 8065 B68A'))))
+        "B845 5B7F FAD5 E8E9 5DEF  4296 637B 0B13 8065 B68A"))))
